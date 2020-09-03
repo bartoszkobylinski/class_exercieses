4419f6cc0ec48c7f4f1e3f5e7f40f560bc13314b
bartek <bartosz.kobylinski@gmail.com>
2020-09-03 16:33:24 +0200

    MOVING TEST CASES TO ANOTHER FILE AND FIXING LINTING ERRORS
    - create test.py file and move all test cases to that file
    
    - fixing some linting errors both in animal and test file

--- animal.py
+++ animal.py
@@ -1,4 +1,3 @@
-import unittest
 
 # 1
 # Write a class representing a 2D point: Point2D. Constructor should have two
@@ -25,20 +24,6 @@ class Point2D:
         return (
             f"Distance beetwen two points is {self.x - x}x and {self.y - y}y")
 
-
-class TestPoint2D(unittest.TestCase):
-
-    def setUp(self):
-        self.point = Point2D(3, 5)
-
-    def test_string_method_of_instance(self):
-        self.assertEqual("Point 3, 5", self.point.__str__())
-
-    def test_calculate_distance_beetwen_two_points(self):
-        self.assertEqual(
-            "Distance beetwen two points is 1x and 2y",
-            self.point.calculate_distance_beetwen_two_points(2, 3))
-
 # Write a class which inherits from Print2D class and implements 3D point,
 # the arguments should be x, y, z write a _str_ method for that class
 # (you can use super) write a method calculating a distance between two points
@@ -59,22 +44,6 @@ class Point3D(Point2D):
         """
         return (f"""Distance beetwen two points is
                 {self.x - x}x {self.y - y}y {self.z -z}z""")
-
-
-class TestPoint3D(unittest.TestCase):
-
-    def setUp(self):
-        self.point = Point3D(2, 8, 5)
-
-    def test_string_method_of_instance(self):
-        self.assertEqual("Point 2, 8, 5", self.point.__str__())
-
-    def test_calculate_distance_beetwen_two_points(self):
-        self.assertEqual(
-                        """Distance beetwen two points is
-                1x 7y 4z""",
-                        self.point.calculate_distance_beetwen_two_points(
-                            1, 1, 1))
 
 # 3
 # Write a Worker class which will take first_name, last_name, salary, level
@@ -130,62 +99,6 @@ class Worker:
         return self.salary
 
 
-class TestWorker(unittest.TestCase):
-
-    def setUp(self):
-        self.worker = Worker(first_name='John',
-                             last_name='Paul',
-                             salary=140,
-                             level=1,
-                             gender='M')
-
-    def test_string_method_worker_instance(self):
-        self.assertEqual(
-"""John Paul,
-                salary: 140,
-                level: 1,
-                gender: M""",
-            self.worker.__str__()
-             )
-
-    def test_raises_error_salary_is_not_integer_when_init(self):
-        with self.assertRaises(ValueError):
-            self.worker.__init__(salary='hundred')
-
-    def test_raises_error_salary_is_integer_lower_then_0_when_init(self):
-        with self.assertRaises(ValueError):
-            self.worker.__init__(salary=(-5200))
-
-    def test_raises_error_level_is_not_integer_when_init(self):
-        with self.assertRaises(ValueError):
-            self.worker.__init__(level='one')
-
-    def test_raises_error_level_is_not_positive_int_when_init(self):
-        with self.assertRaises(ValueError):
-            self.worker.__init__(level=(-99))
-
-    def test_raises_error_gender_is_not_GENDER_choice_when_init(self):
-        with self.assertRaises(ValueError):
-            self.worker.__init__(gender='man')
-
-    def test_increase_workers_salary_when_value(self):
-        self.worker.increase_workers_salary(2000)
-        self.assertEqual(2140, self.worker.salary)
-
-    def test_increase_workers_salary_when_percentage(self):
-        self.worker.increase_workers_salary(percentage=0.1)
-        self.assertEqual(154, self.worker.salary)
-
-    def test_increase_workers_salary_when_value_and_percentage(self):
-        with self.assertRaises(ValueError):
-            self.worker.increase_workers_salary(value=100, percentage=0.2)
-
-    def test_level_up_two_times_worker(self):
-        self.worker.level_up_workers_level()
-        self.worker.level_up_workers_level()
-        self.assertEqual(3, self.worker.level)
-
-
 class Manager(Worker):
 
     def __init__(self, **kwargs):
@@ -201,51 +114,10 @@ class Manager(Worker):
         return worker.salary
 
 
-class TestManager(unittest.TestCase):
-
-    def setUp(self):
-        self.manager = Manager(first_name='Harry',
-                               last_name='Potter',
-                               salary=250,
-                               level=3,
-                               gender='M')
-
-    def test_string_method(self):
-        self.assertEqual(
-"""Manager Harry Potter,
-                salary: 250,
-                level: 3,
-                gender: M""",
-                self.manager.__str__())
-
-    def test_set_salary_to_worker(self):
-        worker = Worker(gender='m')
-        self.manager.set_salary_to_worker(worker, 100)
-        self.assertEqual(100, worker.salary)
-
-    def test_set_salary_to_programmer(self):
-        programmer = Programmer(gender='f')
-        self.manager.set_salary_to_worker(programmer, 500)
-        self.assertEqual(500, programmer.salary)
-
-
-class TestProgrammer(unittest.TestCase):
-    def setUp(self):
-        self.programmer = Programmer(first_name='Anne',
-                                     last_name='Doe',
-                                     salary=250,
-                                     level=0,
-                                     gender='f')
-
-    def test_do_programming(self):
-        code = self.programmer.do_programming()
-        self.assertEqual("<p> look my code is awsome </p>", code)
-
-
 class DataAnalytics(Worker):
 
     DATA_TYPE = ['l', 'd']
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.position = __class__.__name__
@@ -253,7 +125,7 @@ class DataAnalytics(Worker):
     def __str__(self):
         worker_str = super().__str__()
         return f"""{self.position} {worker_str}"""
-    
+
     def create_and_return_data(self, data_type):
         if data_type not in self.DATA_TYPE:
             raise ValueError("""You can choose two data types:
@@ -263,31 +135,6 @@ class DataAnalytics(Worker):
             return ['some data', 'data']
         else:
             return {'somy_key': 'some_value'}
-
-
-class TestDataAnalitycs(unittest.TestCase):
-
-    def setUp(self):
-        self.data_analytics = DataAnalytics(
-            firste_name='Paul',
-            last_name='Smith',
-            level=2,
-            salary=400,
-            gender='m'
-        )
-    
-    def test_create_and_return_data_if_not_valid(self):
-        with self.assertRaises(ValueError):
-            self.data_analytics.create_and_return_data('sh')
-    
-    def test_create_and_return_data_as_list(self):
-        created_list = self.data_analytics.create_and_return_data('l')
-        self.assertIsInstance(created_list, list)
-
-    def test_create_and_return_data_as_dictionary(self):
-        created_dict = self.data_analytics.create_and_return_data('d')
-        self.assertIsInstance(created_dict, dict)
-        
 
 
 class SoftwareDeveloper:
@@ -316,7 +163,3 @@ class SeniorProgrammer(Worker, SoftwareDeveloper):
     def __str__(self):
         worker_str = super().__str__()
         return f"""{self.position} {worker_str}"""
-
-
-if __name__ == "__main__":
-    unittest.main()
