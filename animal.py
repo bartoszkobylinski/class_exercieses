b244faa5b37a12775df069d85d7bd047a9e8161c
bartek <bartosz.kobylinski@gmail.com>
2020-09-03 09:22:04 +0200

    ADDING THREE CLASSES
    - adding Point2D class:
    	- adding init method
    	- adding string method for string represantation of object
    	- adding calculate_distance_beetwen_two_points method for distance calculation
    
    - adding Point3D class which inherit from Point2D class:
    	- adding init method with super() use
    	- adding string method for string represantation of object with super() use
    	- adding calculate_distance_beetwen_two_points method for distance calculation
    
    - adding Worker class:
    	- adding init method (consider putting attributes to kwargs to save some lines)

--- animal.py
+++ animal.py
@@ -1,4 +1,5 @@
 import unittest
+
 # 1
 # Write a class representing a 2D point: Point2D. Constructor should have two
 # arguments x, y define a _str_ method for that class
@@ -39,7 +40,7 @@ class TestPoint2D(unittest.TestCase):
             self.point.calculate_distance_beetwen_two_points(2, 3))
 
 # Write a class which inherits from Print2D class and implements 3D point,
-# the arguments should be x, y, z write a _str_ method for that class 
+# the arguments should be x, y, z write a _str_ method for that class
 # (you can use super) write a method calculating a distance between two points
 
 
@@ -56,8 +57,8 @@ class Point3D(Point2D):
         """
         calculate distance beetwen two points
         """
-        return
-            (f"Distance beetwen two points is {self.x - x}x {self.y - y}y {self.z -z}z")
+        return (f"""Distance beetwen two points is
+                {self.x - x}x {self.y - y}y {self.z -z}z""")
 
 
 class TestPoint3D(unittest.TestCase):
@@ -71,30 +72,28 @@ class TestPoint3D(unittest.TestCase):
     def test_calculate_distance_beetwen_two_points(self):
         self.assertEqual(
                         "Distance beetwen two points is 1x 7y 4z",
-                        self.point.calculate_distance_beetwen_two_points(1, 1, 1))
+                        self.point.calculate_distance_beetwen_two_points(
+                            1, 1, 1))
 
 # 3
-# Write a Worker class which will take first_name, last_name, salary, level and gender
-# write a _str_ and _init_ methods. Add method to level up and a method to increase salary by some value or
-# percentage
-# use inheritance and write a Manager, DataAnalytics, Programmer and SeniorProgrammer
-# write a method in Manager class to set salary for others
-# write a method do_programming for programmers which will return some HTML code in string
-# write a method which will return some data in a list od dictionaries for data scientist
+# Write a Worker class which will take first_name, last_name, salary, level
+# and gender write a _str_ and _init_ methods. Add method to level up and a
+# method to increase salary by some value or percentage use inheritance and
+# write a Manager, DataAnalytics, Programmer and SeniorProgrammer write a
+# method in Manager class to set salary for others write a method
+# do_programming for programmers which will return some HTML code in string
+# write a method which will return some data in a list od dictionaries for
+# data scientist
 
 
 class Worker:
-    
+
     def __init__(self, first_name, last_name, salary, level, gender):
         self.first_name = first_name
         self.last_name = last_name
         self.salary = salary
         self.level = level
         self.gender = gender
-
-
-
-
 
 
 if __name__ == "__main__":
