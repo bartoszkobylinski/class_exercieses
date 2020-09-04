9ab00195f0157d005837c4b85a769179e62f9639
bartek <bartosz.kobylinski@gmail.com>
2020-09-04 18:12:14 +0200

    ADDING TESTS TO TEST WORKER CLASS
    - adding test for string represantation of class and use of parametrize decorator
    
    - adding tests for testing instantiation of class

--- py_test.py
+++ py_test.py
@@ -28,12 +28,39 @@ class TestPoint3D:
         point_name = point.__str__()
         assert point_name == expected
 
-    @pytest.mark.parametrize("distance, expected", [("distance", 11.091)])
-    def test_calculate_distance_beetwen_two_points(self, distance, expected):
+    @pytest.mark.parametrize("x, y, z, expected", [(4, 8, 1, 11.091)])
+    def test_calculate_distance_beetwen_two_points(self, x, y, z, expected):
         point = Point3D(3, 7, 12)
-        distance = point.calculate_distance_beetwen_two_points(4, 8, 1)
-        assert distance == expected
+        #  distance = point.calculate_distance_beetwen_two_points(4, 8, 1)
+        assert point.calculate_distance_beetwen_two_points(x,y,z) == expected
 
 
 class TestWorker:
-    pass
+
+    @pytest.mark.parametrize("expected, kwargs", [(
+        """John Doe,
+                salary: 0,
+                level: 0,
+                gender: m""",
+        {"first_name":"John", "last_name":"Doe", "gender":"m"})
+        ])
+    def test_string_method_class(self, expected, kwargs):
+        worker = Worker(**kwargs)
+        assert str(worker) == expected
+    
+    def test_instance_of_class_error_when_gender_wrong(self):
+        with pytest.raises(ValueError):
+            worker = Worker(gender='bbbb')
+    
+    def test_instance_creation_of_class_fail_of_correct_variable(self):
+        with pytest.raises(ValueError):
+            worker = Worker(gender='f')
+    
+    def test_instance_creation_of_class_when_salary_not_int(self):
+        with pytest.raises(ValueError):
+            worker = Worker(salary="hundred")
+
+    def test_instance_creation_of_class_when_salary_negativ_int(self):
+        with pytest.raises(ValueError):
+            worker = Worker(salary=-(2000))
+
