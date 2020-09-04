f098b2eeec45ee4f7f2ba4aa47910e3f7ba2be67
bartek <bartosz.kobylinski@gmail.com>
2020-09-04 11:23:57 +0200

    MODYFING TEST CASES
    - adding pytest.mark.parametrize decorator to tests

--- py_test.py
+++ py_test.py
@@ -1,23 +1,39 @@
 import pytest
 
-from animal import Point2D, Point3D
+from animal import Point2D, Point3D, Worker
 
 
 class TestPoint2D:
 
-    # @pytest.mark.parametrize("test_string, expected", [(str(test_string),
-    # 'Point 2, 5')])
-    def test_string_method_class(self):
+    @pytest.mark.parametrize("point_name, expected", [("point_name",
+                                                       "Point 2, 5")])
+    def test_string_method_class(self, point_name, expected):
         point = Point2D(2, 5)
-        assert point.__str__() == 'Point 2, 5'
+        point_name = point.__str__()
+        assert point_name == expected
 
-    def test_calculate_distance_beetwen_two_points(self):
+    @pytest.mark.parametrize("distance, expected", [("distance", 8.062)])
+    def test_calculate_distance_beetwen_two_points(self, distance, expected):
         point = Point2D(1, 5)
         distance = point.calculate_distance_beetwen_two_points(8, 9)
-        assert distance == 8.062
+        assert distance == expected
+
 
 class TestPoint3D:
 
-    def test_string_method_class(self):
+    @pytest.mark.parametrize("point_name, expected", [("point_name",
+                                                       "Point 2, 5, 8")])
+    def test_string_method_class(self, point_name, expected):
         point = Point3D(2, 5, 8)
-        assert point.__str__() == 'Point 2, 5, 8'
+        point_name = point.__str__()
+        assert point_name == expected
+
+    @pytest.mark.parametrize("distance, expected", [("distance", 11.091)])
+    def test_calculate_distance_beetwen_two_points(self, distance, expected):
+        point = Point3D(3, 7, 12)
+        distance = point.calculate_distance_beetwen_two_points(4, 8, 1)
+        assert distance == expected
+
+
+class TestWorker:
+    pass
